95e06c115d55885f98f56b97926825d956013d8f
Nikolay <m.nikolay1990@gmail.com>
2023-01-06 19:17:57 +0700

    added routes

---
+++ pages/events/[eventId].js
@@ -0,0 +1,3 @@
+export default function EventId() {
+	return <div>event id</div>;
+}
---
+++ pages/events/index.js
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+useRouter;
+
+export default function EventsPage() {
+	const router = useRouter();
+
+	const events = [
+		{ id: 1, name: 'firstEvent' },
+		{ id: 2, name: 'secondEvent' },
+	];
+
+	function toHomePageHandler() {
+		router.replace('/');
+	}
+
+	return (
+		<div>
+			<h1>Events Page</h1>
+			<ul>
+				{events.map((event) => (
+					<li key={event.id}>
+						<Link
+							href={{
+								pathname: '/events/[eventId]',
+								query: { eventId: event.id },
+							}}
+						>
+							{event.name}
+						</Link>
+					</li>
+				))}
+			</ul>
+			<button onClick={toHomePageHandler}>To Home Page</button>
+		</div>
+	);
+}
